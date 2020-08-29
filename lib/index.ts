df52a678f915cd663420ec00a15007f029fb4804
Min Cai <min.cai.china@gmail.com>
2020-08-29 17:22:22 +0800

    Add publish.sh.

--- lib/index.ts
+++ lib/index.ts
@@ -1,27 +1,29 @@
 import express from 'express';
 import cors from 'cors';
 
-const app = express();
+function startServer() {
+    const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
 
-app.use(cors());
+    app.use(cors());
 
-app.get('/', (req, res) => {
-    return res.send('Received a GET HTTP method');
-});
+    app.get('/', (req, res) => {
+        return res.send('Received a GET HTTP method');
+    });
 
-app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method');
-});
+    app.post('/', (req, res) => {
+        return res.send('Received a POST HTTP method');
+    });
 
-app.get('*', function(req, res){
-    return res.status(404).send(`404 error: not found: ${req.url}`);
-});
+    app.get('*', function(req, res){
+        return res.status(404).send(`404 error: not found: ${req.url}`);
+    });
 
-let port = 5001;
+    let port = 5001;
 
-app.listen(port, () =>
-    console.log(`nesting listening on port ${port}!`),
-);
+    app.listen(port, () =>
+        console.log(`nesting listening on port ${port}!`),
+    );
+}
---
+++ publish.sh
@@ -0,0 +1,1 @@
+tsc && npm publish
