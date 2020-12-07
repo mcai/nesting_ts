95aa38264a0bd89ee15c34ceabb51c8d56dd9f97
Min Cai <min.cai.china@gmail.com>
2020-12-07 19:49:16 +0800

    Update GPU stuffs.

--- src/geometry/nfp.ts
+++ src/geometry/nfp.ts
@@ -1,12 +1,7 @@
-import { Line, lineLength, Point, pointInPolygon, pointOnLine, Polygon, polygonBounds } from "geometric";
+import { Point, pointInPolygon, Polygon, polygonBounds } from "geometric";
 import { NoFitRasterGpuCalculatorHelper } from "../gpu/NoFitRasterGpuCalculatorHelper";
 
-export const origin: Point = [0.0, 0.0];
-
 export type Vector = [number, number];
-
-export const xAxis: Vector = [1.0, 0.0];
-export const yAxis: Vector = [0.0, 1.0];
 
 export function pointAddVector(a: Point, b: Vector): Point {
     return [a[0] + b[0], a[1] + b[1]];
@@ -20,51 +15,15 @@ export function vectorTo(from: Point, to: Point): Vector {
     return [to[0] - from[0], to[1] - from[1]];
 }
 
-export function vectorNormalize(vector: Vector): Vector {
-    const length = lineLength([origin, vector]);
-    return [vector[0] / length, vector[1] / length];
-}
-
-export function vectorMultiply(vector: Vector, a: number): Vector {
-    return [vector[0] * a, vector[1] * a];
-}
-
-export function lineDirection(line: Line): Vector {
-    return vectorNormalize(vectorTo(line[0], line[1]));
-}
-
-export function lineSplit(line: Line): Point[] {
-    let points = [line[0]];
-
-    let point = pointAddVector(line[0], lineDirection(line));
-
-    while (pointOnLine(point, line)) {
-        points = [...points, point];
-        point = pointAddVector(point, lineDirection(line));
-    }
-
-    return points;
-}
-
-export function getRasterPoints(boardBounds: [Point, Point]): Point[] {
+export function getRasterPoints(bounds: [Point, Point]): Point[] {
     let dots: Point[] = [];
 
-    let startPoint: Point = [boardBounds[0][0], boardBounds[1][1]];
-    let endPoint = boardBounds[1];
+    const boundsSize = vectorTo(bounds[0], bounds[1]);
 
-    let i = 0;
-
-    while (startPoint[1] >= boardBounds[0][1]) {
-        const line: Line = i % 2 == 0 ? [startPoint, endPoint] : [endPoint, startPoint];
-
-        const split = lineSplit(line);
-
-        dots = [...dots, ...split];
-
-        startPoint = pointSubtractVector(startPoint, yAxis);
-        endPoint = pointSubtractVector(endPoint, yAxis);
-
-        i++;
+    for (let i = 0; i <= boundsSize[0]; i++) {
+        for (let j = 0; j <= boundsSize[1]; j++) {
+            dots = [...dots, pointAddVector(bounds[0], [i, j])];
+        }
     }
 
     return dots;
